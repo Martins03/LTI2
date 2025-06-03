f13fc3094b6381ee1d323d95fe9419d156f73055
2211441@my.ipleiria.pt <97673890+Martins03@users.noreply.github.com>
2025-06-03 15:34:22 +0100

    Change

--- LTI2/LTI2/LTI2/LoginForm.Designer.cs
+++ LTI2/LTI2/LTI2/LoginForm.Designer.cs
@@ -5,11 +5,9 @@
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.TextBox txtIP;
         private System.Windows.Forms.TextBox txtPort;
-        private System.Windows.Forms.TextBox txtToken;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lblIP;
         private System.Windows.Forms.Label lblPort;
-        private System.Windows.Forms.Label lblToken;
 
         protected override void Dispose(bool disposing)
         {
@@ -21,39 +19,26 @@
         {
             this.txtIP = new System.Windows.Forms.TextBox();
             this.txtPort = new System.Windows.Forms.TextBox();
-            this.txtToken = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.lblIP = new System.Windows.Forms.Label();
             this.lblPort = new System.Windows.Forms.Label();
-            this.lblToken = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtIP
             // 
             this.txtIP.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtIP.Location = new System.Drawing.Point(194, 41);
+            this.txtIP.Location = new System.Drawing.Point(194, 71);
             this.txtIP.Name = "txtIP";
             this.txtIP.Size = new System.Drawing.Size(230, 43);
             this.txtIP.TabIndex = 1;
-            this.txtIP.Text = "";
             // 
             // txtPort
             // 
             this.txtPort.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtPort.Location = new System.Drawing.Point(194, 102);
+            this.txtPort.Location = new System.Drawing.Point(194, 160);
             this.txtPort.Name = "txtPort";
             this.txtPort.Size = new System.Drawing.Size(230, 43);
             this.txtPort.TabIndex = 3;
-            this.txtPort.Text = "";
-            // 
-            // txtToken
-            // 
-            this.txtToken.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtToken.Location = new System.Drawing.Point(194, 166);
-            this.txtToken.Multiline = true;
-            this.txtToken.Name = "txtToken";
-            this.txtToken.Size = new System.Drawing.Size(230, 40);
-            this.txtToken.TabIndex = 5;
             // 
             // btnLogin
             // 
@@ -72,7 +57,7 @@
             // lblIP
             // 
             this.lblIP.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.lblIP.Location = new System.Drawing.Point(64, 41);
+            this.lblIP.Location = new System.Drawing.Point(64, 71);
             this.lblIP.Name = "lblIP";
             this.lblIP.Size = new System.Drawing.Size(128, 43);
             this.lblIP.TabIndex = 0;
@@ -81,21 +66,11 @@
             // lblPort
             // 
             this.lblPort.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.lblPort.Location = new System.Drawing.Point(64, 102);
+            this.lblPort.Location = new System.Drawing.Point(64, 160);
             this.lblPort.Name = "lblPort";
             this.lblPort.Size = new System.Drawing.Size(100, 43);
             this.lblPort.TabIndex = 2;
             this.lblPort.Text = "Porta:";
-            // 
-            // lblToken
-            // 
-            this.lblToken.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.lblToken.Location = new System.Drawing.Point(64, 166);
-            this.lblToken.Name = "lblToken";
-            this.lblToken.Size = new System.Drawing.Size(96, 47);
-            this.lblToken.TabIndex = 4;
-            this.lblToken.Text = "Token:";
-            this.lblToken.Click += new System.EventHandler(this.lblToken_Click);
             // 
             // LoginForm
             // 
@@ -104,8 +79,6 @@
             this.Controls.Add(this.txtIP);
             this.Controls.Add(this.lblPort);
             this.Controls.Add(this.txtPort);
-            this.Controls.Add(this.lblToken);
-            this.Controls.Add(this.txtToken);
             this.Controls.Add(this.btnLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
